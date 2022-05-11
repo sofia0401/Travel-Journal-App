ca7b45d77efb8e203cdec74f9c003a92aa573ab2
sofia0401 <sofia0401@yandex.ru>
2022-05-11 21:27:02 +0300

    fix(*): env fix

--- index.js
+++ index.js
@@ -29,6 +29,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 const PORT = process.env.PORT || 5000;
-mongoose.connect(process.env.CONNECTION_URL)
+mongoose.connect(process.env.MONGODB_URI)
     .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
     .catch((error) => console.error(error.message));
